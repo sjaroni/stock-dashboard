76dd47118c78735e8bf25319963d445776da587d
Stefan Jaroni <sjaroni@outlook.de>
2024-09-04 20:14:49 +0200

    style: add box no investment advice

--- src/components/HeadLine.vue
+++ src/components/HeadLine.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="headLine">
     <div class="rectangle"></div>
-    <div class="text">The Magnificent Seven Companies</div>
+    <div class="text">
+      The Magnificent Seven Companies
+      <div class="info">
+        <img src="../assets/icons/info.svg" alt="Info Icon" />
+        <div id="info-text">
+          <div id="close">X</div>
+          <div class="info-text-content">
+            Die Kurse spiegeln nicht alle Märkte wider sind möglicherweise bis
+            zu 20 Minuten zeitverzögert. Die Angaben werden ohne Mängelgewähr
+            zur Verfügung gestellt. Sie dienen nur zur Information und sind
+            nicht zu Handels- oder Beratungszwecken zu verwenden.
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +30,7 @@ export default {
   position: absolute;
   left: 0;
   top: 64px;
-  width: 714px;
+  width: 1324px;
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -35,5 +49,50 @@ export default {
   font-size: 36px;
   line-height: 42.66px;
   color: #f9f9f9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  .info {    
+    text-align: right;
+    position: relative;
+    width: 100%;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 20px;
+
+    img {
+      height: 32px;
+      width: 32px;
+      border-radius: 50%;
+      border: 2px solid transparent;
+      &:hover {
+        border: 2px solid #39daff;
+        cursor: pointer;
+      }
+    }
+
+    #info-text {
+      position: absolute;
+      left: 50%;
+      transform: translate(-75%, 0);
+      height: 20vh;
+      width: 50%;
+      background: #39daff;
+      border-radius: 16px;
+      z-index: 3;
+      padding: 40px;
+      text-align: center;
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: column;
+      gap: 32px;
+
+      #close {
+        text-align: right !important;
+      }
+    }
+  }
 }
 </style>
